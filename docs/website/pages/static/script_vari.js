95f5408b9d39f368990c59b53b6f606796a17cb0
MissCrispenCakes <10444589+MissCrispenCakes@users.noreply.github.com>
2022-04-30 23:02:40 -0400

    Update script_vari.js

--- docs/website/pages/static/script_vari.js
+++ docs/website/pages/static/script_vari.js
@@ -89,7 +89,7 @@ function renderPlaces(places) {
        
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', places.url);
+        model.setAttribute('gltf-model', place.url);
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.05 0.05 0.05');
