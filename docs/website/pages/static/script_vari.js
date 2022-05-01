987c511506e7d27d717b085716af846bb3200b85
MissCrispenCakes <10444589+MissCrispenCakes@users.noreply.github.com>
2022-05-01 06:24:39 -0400

    Update script_vari.js

--- docs/website/pages/static/script_vari.js
+++ docs/website/pages/static/script_vari.js
@@ -108,7 +108,7 @@ function renderPlaces(places) {
         let check = getPosition();
         if (check) { // == 'lat: 43.773071, lng: -79.503404,') {
 //         if(check.lat){
-            console.log(`checks out!: `we are on the way`);
+            console.log(`checks out!: we are on the way`);
             console.log(`GPS CHECK -- current: Lat ${currentLat} Lon ${currentLng} ::: start: Lat ${startLat} Lon ${startLng} ::: check: ${check.lat} ${check.long}`);
             
             document.querySelector('button[data-action="change"]').addEventListener('click', function () {
