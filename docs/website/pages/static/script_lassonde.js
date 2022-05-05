195ed377f8f104cdde5b6d05339b5e8a63cbedf6
Snow-Lotus-1 <lujason310@gmail.com>
2022-05-05 03:10:34 -0400

    Basically example, tf is wrong with distance

--- docs/website/pages/static/script_lassonde.js
+++ docs/website/pages/static/script_lassonde.js
@@ -1,11 +1,9 @@
-let startLat;
-let startLng;
-let places
+let startLat, startLng, currentLat, currentLng;
+let places;
 const LAT_LONG_SECOND = 1/60/60;
 const FEET_PER_LAT_SECOND = 100;
 const FEET_PER_LONG_SECOND = 75;
-let currentLat;
-let currentLng;
+const DISTANCE_IN_FEET = 30; // distance for object to appear
 
 window.onload = () => { 
     const button = document.querySelector('button[data-action="change"]');
@@ -17,7 +15,7 @@ window.onload = () => {
     places = loadPlaces();
     console.log('Hello');
 };
- 
+
 function updatePosition() {
     if(navigator.geolocation) {
         navigator.geolocation.watchPosition(
@@ -25,21 +23,21 @@ function updatePosition() {
                 currentLng = position.coords.longitude;
                 currentLat = position.coords.latitude;
                 console.log(`Current Lat ${position.coords.latitude} Lon ${position.coords.longitude}`);
- 
+
                 places.forEach(place => {
                     let latFromPlace = Math.abs(currentLat - place.location.lat);
                     let longFromPlace = Math.abs(currentLng - place.location.lng);
- 
+
                     let latSeconds = latFromPlace / LAT_LONG_SECOND;
                     let longSeconds = longFromPlace / LAT_LONG_SECOND;
                     let latFeet = latSeconds * FEET_PER_LAT_SECOND;
                     let longFeet = longSeconds * FEET_PER_LONG_SECOND;
- 
-                    if (latFeet <= 15 && longFeet <= 15) {
-                        console.log("in range");
+
+                    if (latFeet <= DISTANCE_IN_FEET && longFeet <= DISTANCE_IN_FEET) {
+                        console.log("In range!");
                         renderPlace(place);
                     } else {
-                        console.log("not in range");
+                        console.log("Not in range");
                     }
                 });
             },
@@ -54,7 +52,7 @@ function updatePosition() {
         alert("Sorry, geolocation not supported in this browser");
     }
 };
- 
+
 function getStartingPosition() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
@@ -74,8 +72,8 @@ function getStartingPosition() {
         alert("Sorry, geolocation not supported in this browser");
     }
 }   
- 
- 
+
+
 function loadPlaces() {
     return [ 
         {
@@ -90,16 +88,17 @@ function loadPlaces() {
                 short: 'Name: Lassonde Building LSA (formerly Computer Science & Engineering Building)',
                 loc: 'Location: 120 Campus Walk',
                 hour: 'Building operating hours: 7:00am to 9:00pmMonday to Friday, weekends building is locked 24hr',
+                blank: ' '
             },
         },
     ]
 };
- 
+
 var infoIdx = 0;
 function renderPlace(place) {
     let scene = document.querySelector('a-scene');
     let div = document.querySelector('.instructions');
- 
+
     let shorthand = place.info.short;
     let location = place.info.loc;
     let hours = place.info.hour;
@@ -113,11 +112,11 @@ function renderPlace(place) {
     model.setAttribute('name', place.name);
     model.setAttribute('info', '');
     model.setAttribute('position', '0 0 -20');
- 
+
     document.querySelector('button[data-action="change"]').addEventListener('click', function () {
         var el = document.getElementById(model.id);
         var newIdx = infoIdx % 3;
- 
+
         if (newIdx === 1) {
             el.setAttribute('info', { event: 'updateInfo', message: shorthand });
             el.emit('updateInfo');
@@ -133,6 +132,6 @@ function renderPlace(place) {
         }
         infoIdx++;
     });
- 
+
     scene.appendChild(model);
 }
